74b4561e8e5116e4e1bf7887f5b2d66fe3cb0561
G'lek Tarssza <glek@glektarssza.com>
2025-06-05 09:28:11 -0600

    Add some basic Java sources.

---
+++ src/main/java/com/glektarssza/gradle/conventions/ProjectConventionsPlugin.java
@@ -0,0 +1,18 @@
+package com.glektarssza.gradle.conventions;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ProjectConventionsPlugin implements Plugin<Project> {
+    /**
+     * Apply the conventions to the given {@link Project} object.
+     *
+     * @param target The target object to apply the conventions to.
+     */
+    @Override
+    public void apply(final @NotNull Project target) {
+        // TODO: Implement
+    }
+}
---
+++ src/main/java/com/glektarssza/gradle/conventions/SettingsConventionsPlugin.java
@@ -0,0 +1,18 @@
+package com.glektarssza.gradle.conventions;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.initialization.Settings;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SettingsConventionsPlugin implements Plugin<Settings> {
+    /**
+     * Apply the conventions to the given {@link Settings} object.
+     *
+     * @param target The target object to apply the conventions to.
+     */
+    @Override
+    public void apply(final @NotNull Settings target) {
+        // TODO: Implement
+    }
+}
